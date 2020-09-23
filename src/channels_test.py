8b393ffc718cdc286e5c8c2f8ea4fe2110925ecd
Ricky <z5310067@ad.unsw.edu.au>
2020-09-23 16:56:53 +1000

    Added function 'test_channels_listall' to code

--- src/channels_test.py
+++ src/channels_test.py
@@ -4,4 +4,6 @@ from error import InputError
 
 def test_channels_list():
     assert channels.channels_list('token') == 'channels'
-
+    
+def test_hannels_listall():
+    assert channels.channels_list('token') == 'channels'
