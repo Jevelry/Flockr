5544133600a31f291c5c1c01cf8b65d35783f203
Ricky <z5310067@ad.unsw.edu.au>
2020-09-23 18:06:08 +1000

    Added functions test_channels_create and test_channels_create_except to code

--- src/channels_test.py
+++ src/channels_test.py
@@ -5,5 +5,12 @@ from error import InputError
 def test_channels_list():
     assert channels.channels_list('token') == 'channels'
     
-def test_hannels_listall():
+def test_channels_listall():
     assert channels.channels_list('token') == 'channels'
+
+def test_channels_create():
+    result = channels.channels_create(token, name, is_public)
+
+def test_channels_create_except():
+    with pytest.raises(InputError) as e:
+        assert len(name) > 20
