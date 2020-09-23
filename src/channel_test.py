27c9824801f669c6c49d4701c8f7a18d4eeb8a68
Kevin <z5311917@ad.unsw.edu.au>
2020-09-23 20:37:04 +1000

    Createt channel_test branch and begun writing test for channel_details

---
+++ src/channel_test.py
@@ -0,0 +1,26 @@
+import channel
+import pytest
+from error import InputError
+from error import AccessError
+
+def test_channel_invite():
+    
+    
+    
+       
+
+
+
+def test_channel_invite_excpet():
+    with pytest.raises(InputError) as e:
+        
+
+
+
+
+def test_channel_details():
+    auth_register(best_group123@gmail.com, awesome, best, group)
+    auth_register(bestest_group123@gmail.com, awesome, bestest, group)
+    channel_id = channels_create(best_group123@gmail.com, temp_channel, 0) 
+    channel_invite(best_group123@gmail.com, channel_id, 2)
+    assert channel_details(bestest_group123@gmail.com, channel_id)
