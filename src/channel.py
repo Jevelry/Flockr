d7f7f031cb4973f5c531fc6996542bf567d57544
Kevin <z5311917@ad.unsw.edu.au>
2020-09-27 16:40:04 +1000

    finished rudimentary version of channel.py, unsure if it works yet

--- src/channel.py
+++ src/channel.py
@@ -6,18 +6,18 @@ import channels
 
 # Used in channel_invite
 # Check if token is valid and if user is authorised user
-def valid_token(token,channel_id):
+def valid_token(token, channel_id):
     u_id = None
     # Check valid token, take u_id
-    for user in data.logged_in:
+    for user in data.data["logged_in"]:
         if user["token"] == token:
             u_id = user["u_id"]
     if u_id = None:
         return False
     # Check if user is existing member of channel
-    for user in data.users:
+    for user in data.data["users"]:
         if user["u_id"] == u_id:
-            for channel in user[channel_list]:
+            for channel in user["channel_list"]:
                 if channel["channel_id"] == channel_id
                 return True
             return False
@@ -26,7 +26,7 @@ def valid_token(token,channel_id):
 # Used in channel_invite
 # Check if valid channel id
 def valid_channel_id(channel_id):
-    for channel in data.channels:
+    for channel in data.data["channels"]:
         if channel["channel_id"] == channel_id
             return True
     return False
@@ -36,22 +36,43 @@ def valid_channel_id(channel_id):
 # Used in channel_invite
 # Check if valid u_id of invitee
 def valid_u_id(u_id):
-    
-
+    for user in data.data["users"]:
+        if user["u_id"] == u_id:
+            return True
+        return False
+                
+def is_existing_channel_member(u_id, channel_id):
+    for user in data.data["users"]:
+        if user["u_id"] == u_id:
+            for channel in user["channel_list"]:
+                if channel == channel_id:
+                    return True
+    return False
 
 
 def channel_invite(token, channel_id, u_id):
+    # Check if given valid channel_id
+    if not valid_channel_id(token, channel_id):
+        raise InputError
+    # Check if token is valid and user is authorised(member of channel)    
+    if not valid_token(token, channel_id):
+        raise AccessError
+    # Check if given valid u_id    
+    if not valid_u_id(u_id):
+        raise InputError
+    # Check if invitee is already part of channel    
+    if is_existing_channel_member(u_id, channel_id)
+        return{
+        }
     
-    
-    
-
-
-
-
-
-
-
-
+    # Everything valid, Proceed with adding to channel
+    for channel in data.data["channels"]:
+        if channel["channel_id"] == channel_id:
+            channel["members"].append(u_id)
+            
+    for user in data.data["users"]:
+        if user["u_id"] == u_id:
+            user["channel_list"].append(channel_id
 
     return {
     }
