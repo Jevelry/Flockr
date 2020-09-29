2b24fc672f206c8d2408b4123455f43573b416bf
Ricky <z5310067@ad.unsw.edu.au>
2020-09-29 22:13:04 +1000

    Edited function

--- src/channels.py
+++ src/channels.py
@@ -5,13 +5,12 @@ def channels_list(token):
 
     for user in data.data['logged_in']:
         if user['token'] == token:
-            global u_id 
             u_id = user['u_id']
 
-    for channel in data.data['channels']:
-        for owner_u_id in channel['owners']:
-            if owner_u_id == u_id:
-                channels_list.append(channel)
+        for channel in data.data['channels']:
+            for owner_u_id in channel['owners']:
+                if owner_u_id == u_id:
+                    channels_list.append(channel)
         
         for member_u_id in channel['members']:
             if member_u_id == u_id:
@@ -31,15 +30,14 @@ def channels_listall(token):
 def channels_create(token, name, is_public):
     for user in data.data['logged_in']:
         if user['token'] == token:
-            global u_id 
             u_id = user['u_id']
     
-    new_channel = {
-        'channel_id' : (len(data.data['channels']) + 1),
-        'name' : name,
-        'state' : is_public,
-        'owners' : [u_id],
-    }
-    data.data['channels'].append(new_channel)
+        new_channel = {
+            'channel_id' : (len(data.data['channels']) + 1),
+            'name' : name,
+            'state' : is_public,
+            'owners' : [u_id],
+        }
+        data.data['channels'].append(new_channel)
 
     return {'is_success' : True}
