faae6633ba00365fa5075aead653e3e27bf79e40
Ricky <z5310067@ad.unsw.edu.au>
2020-09-29 21:45:35 +1000

    Added draft implementation of channels.py

--- src/channels.py
+++ src/channels.py
@@ -1,24 +1,43 @@
-def channels_list(token):
-    return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
-    }
+import data
 
+def channels_list(token):
+    channels_list = []
+
+    for user in data.data['logged_in']:
+        if user['token'] == token:
+            u_id = user['u_id']
+
+    for channel in data.data['channels']:
+        for owner_u_id in channel['owners']:
+            if owner_u_id == u_id:
+                channels_list.append(channel)
+        
+        for member_u_id in channel['members']:
+            if member_u_id == u_id:
+                channels_list.append(channel)
+
+    for channel in channels_list:
+        print(channel)
+
+    return channels_list
+ 
 def channels_listall(token):
-    return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
-    }
+    for channel in data.data['channels']:
+        print(channel)
+    
+    return data.data['channels']
 
 def channels_create(token, name, is_public):
-    return {
-        'channel_id': 1,
+    for user in data.data['logged_in']:
+        if user['token'] == token:
+            u_id = user['u_id']
+    
+    new_channel = {
+        'channel_id' : (len(data.data['channels']) + 1),
+        'name' : name,
+        'state' : is_public,
+        'owners' : [u_id],
     }
+    data.data['channels'].append(new_channel)
+
+    return {'is_success' : True}
--- src/channels_test.py
+++ src/channels_test.py
@@ -2,73 +2,88 @@ import auth
 import channel
 import channels
 import pytest
+import data
 from error import InputError
 
 # Tests for channels_list:
 def test_channels_list():
+    data.clear_data()
+
     user = auth.auth_register('testmail@gmail.com', 'password', 'first_name', 'last_name')
     channels.channels_create(user['token'], 'test_channel', True)
     list_result = channels.channels_list(user['token'])
-    assert list_result['channels'][0]['channel_id'] == 1
-    assert list_result['channels'][0]['name'] == 'test_channel'
+    assert list_result[0]['channel_id'] == 1
+    assert list_result[0]['name'] == 'test_channel'
 
     user2 = auth.auth_register('another_test@hotmail.com', 'password123', 'first_name', 'last_name')
     channels.channels_create(user2['token'], 'test_channel_two', True)
     channels.channels_create(user2['token'], 'test_channel_three', True)
     list_result2 = channels.channels_list(user2['token'])
-    assert list_result2['channels'][0]['channel_id'] == 2
-    assert list_result2['channels'][0]['name'] == 'test_channel_two'
-    assert list_result2['channels'][1]['channel_id'] == 3
-    assert list_result2['channels'][1]['name'] == 'test_channel_three'
+    assert list_result2[0]['channel_id'] == 2
+    assert list_result2[0]['name'] == 'test_channel_two'
+    assert list_result2[1]['channel_id'] == 3
+    assert list_result2[1]['name'] == 'test_channel_three'
+
+    data.clear_data()
 
 # Tests for channels_listall:
 def test_channels_listall():
+    data.clear_data()
+
     user = auth.auth_register('abc123@gmail.com', 'passwordabc', 'first_name', 'last_name')
     channels.channels_create(user['token'], 'my_channel', True)
     list_result = channels.channels_listall(user['token'])
-    assert list_result['channels'][0]['channel_id'] == 1
-    assert list_result['channels'][0]['name'] == 'my_channel'
+    assert list_result[0]['channel_id'] == 1
+    assert list_result[0]['name'] == 'my_channel'
     
     user2 = auth.auth_register('xyz456@gmail.com', 'passwordxyz', 'first_name', 'last_name')
     channel.channel_join(user2['token'], ' my_channel')
     channels.channels_create(user2['token'], 'our_channel', True)
     list_result2 = channels.channels_listall(user2['token'])
-    assert list_result2['channels'][0]['channel_id'] == 1
-    assert list_result2['channels'][0]['name'] == 'my_channel'
-    assert list_result2['channels'][1]['channel_id'] == 2
-    assert list_result2['channels'][1]['name'] == 'our_channel'
+    assert list_result2[0]['channel_id'] == 1
+    assert list_result2[0]['name'] == 'my_channel'
+    assert list_result2[1]['channel_id'] == 2
+    assert list_result2[1]['name'] == 'our_channel'
     
+    data.clear_data()
+
 # Tests for channels_create:
 # Successful
 def test_channels_create_name_valid():
+    data.clear_data()
+
     user = auth.auth_register('testmail@gmail.com', 'passwordpassword', 'first_name', 'last_name')
     channels.channels_create(user['token'], 'test_name', True)
     list_result = channels.channels_list(user['token'])
-    assert list_result['channels'][0]['channel_id'] == 1
-    assert len(list_result['channels'][0]['name']) < 20
+    assert list_result[0]['channel_id'] == 1
+    assert len(list_result[0]['name']) < 20
 
     user2 = auth.auth_register('mailtest@gmail.com', 'passwordword', 'first_name', 'last_name')
     channels.channels_create(user2['token'], 'test_name_two', False)
     list_result2 = channels.channels_list(user2['token'])
-    assert list_result2['channels'][0]['channel_id'] == 2
-    assert len(list_result2['channels'][0]['name']) < 20
+    assert list_result2[0]['channel_id'] == 2
+    assert len(list_result2[0]['name']) < 20
 
-    
+    data.clear_data()
+
 # Unsuccessful
 def test_channels_create_name_error():
+    data.clear_data()
+    
     user = auth.auth_register('testmail123@gmail.com', 'passwordpass', 'first_name', 'last_name')
     channels.channels_create(user['token'], 'test_name_12345678910x', True)
     list_result = channels.channels_list(user['token'])
-    assert list_result['channels'][0]['channel_id'] == 1
+    assert list_result[0]['channel_id'] == 1
 
     with pytest.raises(InputError) as e:
-        assert len(list_result['channels'][0]['name']) > 20
+        assert len(list_result[0]['name']) > 20
 
     user2 = auth.auth_register('mailtest123@gmail.com', 'passwpassword', 'first_name', 'last_name')
     channels.channels_create(user2['token'], 'test_name_0987654321x', False)
     list_result2 = channels.channels_list(user2['token'])
-    assert list_result2['channels'][0]['channel_id'] == 2
+    assert list_result2[0]['channel_id'] == 2
 
     with pytest.raises(InputError) as e:
-        assert len(list_result2['channels'][0]['name']) > 20
+        assert len(list_result2[0]['name']) > 20
 
+    data.clear_data()
