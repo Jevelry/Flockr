0319c4c3c33dfa816b07bfcd542284a43ee1ebdb
Ricky <z5310067@ad.unsw.edu.au>
2020-10-01 17:06:55 +1000

    added more tests; annotated functions

--- src/channels.py
+++ src/channels.py
@@ -1,14 +1,22 @@
 import data
 from error import InputError
+from error import AccessError
 
 def channels_list(token):
+    # Initialise channel_list to return to user
     channels_list = []
+    u_id = None
 
-    for user in data.data['logged_in']:
+    # Finds u_id associated with user token
+    # Returns AccessError if token does not exist
+    for user in data.data['logged_in']:            
         if user['token'] == token:
-            global u_id
             u_id = user['u_id']
+        
+    if u_id == None:
+        raise AccessError('Token does not exist!')
 
+    # Appends all channels that the user is in to channel_list
     for channel in data.data['channels']:
         for owner_id in channel['owners']:
             if owner_id == u_id:
@@ -29,8 +37,20 @@ def channels_list(token):
     return channels_list
  
 def channels_listall(token):
+    # Initialise channel_list to return to user
     channels_list = []
 
+    # Finds u_id associated with user token
+    # Returns AccessError if token does not exist
+    u_id = None
+    for user in data.data['logged_in']:            
+        if user['token'] == token:
+            u_id = user['u_id']
+        
+    if u_id == None:
+        raise AccessError('Token does not exist!')
+
+    # Appends channel_id and name of all channels into channel_list
     for channel in data.data['channels']:
         channel_copy = {
             'channel_id' : channel['channel_id'],
@@ -41,14 +61,21 @@ def channels_listall(token):
     return channels_list
 
 def channels_create(token, name, is_public):
+    # Returns InputError if channel name is more than 20 characters
     if len(name) > 20:
         raise InputError('Name cannot be more than 20 characters long')
 
+    # Finds u_id associated with user token
+    # Returns AccessError if token does not exist
+    u_id = None
     for user in data.data['logged_in']:
         if user['token'] == token:
-            global u_id
             u_id = user['u_id']
+
+    if u_id == None:
+        raise AccessError('Token does not exist!')
     
+    # Creates a new channel and stores to the 'channels' in data.py
     new_channel = {
         'channel_id' : (len(data.data['channels']) + 1),
         'name' : name,
@@ -60,6 +87,11 @@ def channels_create(token, name, is_public):
     }
     channel_copy = new_channel.copy()
     data.data['channels'].append(channel_copy)
+
+    # Stores channel as part of the user's channel list
+    for user in data.data['users']:
+        if user['u_id'] == u_id:
+            user['channel_list'].append(channel_copy)
 
     return {
         'channel_id': new_channel['channel_id'],
--- src/channels_test.py
+++ src/channels_test.py
@@ -3,21 +3,22 @@ import channel
 import channels
 import pytest
 import data
+import other
 from error import InputError
+from error import AccessError
 
 # Tests for channels_list:
-def test_channels_list():
-    data.clear_data()
+# Successful
+def test_channels_list_valid_token():
+    other.clear()
 
     user = auth.auth_register('testmail@gmail.com', 'password', 'first_name', 'last_name')
-    user = auth.auth_login('testmail@gmail.com', 'password')
     channels.channels_create(user['token'], 'test_channel', True)
     list_result = channels.channels_list(user['token'])
     assert list_result[0]['channel_id'] == 1
     assert list_result[0]['name'] == 'test_channel'
 
     user2 = auth.auth_register('another_test@hotmail.com', 'password123', 'first_name', 'last_name')
-    user2 = auth.auth_login('another_test@hotmail.com', 'password123')
     channels.channels_create(user2['token'], 'test_channel_two', True)
     channels.channels_create(user2['token'], 'test_channel_three', True)
     list_result2 = channels.channels_list(user2['token'])
@@ -26,21 +27,37 @@ def test_channels_list():
     assert list_result2[1]['channel_id'] == 3
     assert list_result2[1]['name'] == 'test_channel_three'
 
-    data.clear_data()
+    other.clear()
+
+# Unsuccessful
+def test_channels_list_invalid_token():
+    other.clear()
+
+    user = auth.auth_register('testmailtest@gmail.com', 'password121', 'first_name', 'last_name')
+    channels.channels_create(user['token'], 'test_channel', True)
+    with pytest.raises(AccessError) as e:
+        assert channels.channels_list('invalid_token')
+
+    user2 = auth.auth_register('another_testtwo@hotmail.com', 'password12321', 'first_name', 'last_name')
+    channels.channels_create(user2['token'], 'test_channel_two', True)
+    channels.channels_create(user2['token'], 'test_channel_three', True)
+    with pytest.raises(AccessError) as e:
+        assert channels.channels_list('another_invalid_token')
+
+    other.clear()
 
 # Tests for channels_listall:
-def test_channels_listall():
-    data.clear_data()
+# Successful
+def test_channels_listall_valid_token():
+    other.clear()
 
     user = auth.auth_register('abc123@gmail.com', 'passwordabc', 'first_name', 'last_name')
-    user = auth.auth_login('abc123@gmail.com', 'passwordabc')
     channels.channels_create(user['token'], 'my_channel', True)
     list_result = channels.channels_listall(user['token'])
     assert list_result[0]['channel_id'] == 1
     assert list_result[0]['name'] == 'my_channel'
     
     user2 = auth.auth_register('xyz456@gmail.com', 'passwordxyz', 'first_name', 'last_name')
-    user2 = auth.auth_login('xyz456@gmail.com', 'passwordxyz')
     channel.channel_join(user2['token'], ' my_channel')
     channels.channels_create(user2['token'], 'our_channel', True)
     list_result2 = channels.channels_listall(user2['token'])
@@ -51,39 +68,66 @@ def test_channels_listall():
     
     data.clear_data()
 
+# Unsuccessful
+def test_channels_listall_invalid_token():
+    other.clear()
+
+    user = auth.auth_register('abc123abc@gmail.com', 'passwordabc1', 'first_name', 'last_name')
+    channels.channels_create(user['token'], 'my_channel', True)
+    with pytest.raises(AccessError) as e:
+        assert channels.channels_listall('invalid_token')
+    
+    user2 = auth.auth_register('xyz456xyz@gmail.com', 'passwordxyz1', 'first_name', 'last_name')
+    channel.channel_join(user2['token'], ' my_channel')
+    channels.channels_create(user2['token'], 'our_channel', True)
+    with pytest.raises(AccessError) as e:
+        assert channels.channels_listall('another_invalid_token')
+
+    data.clear_data()
+
 # Tests for channels_create:
 # Successful
 def test_channels_create_name_valid():
-    data.clear_data()
+    other.clear()
 
     user = auth.auth_register('testmail@gmail.com', 'passwordpassword', 'first_name', 'last_name')
-    user = auth.auth_login('testmail@gmail.com', 'passwordpassword')
     channels.channels_create(user['token'], 'test_name', True)
     list_result = channels.channels_list(user['token'])
     assert list_result[0]['channel_id'] == 1
     assert len(list_result[0]['name']) < 20
 
     user2 = auth.auth_register('mailtest@gmail.com', 'passwordword', 'first_name', 'last_name')
-    user2 = auth.auth_login('mailtest@gmail.com', 'passwordword')
     channels.channels_create(user2['token'], 'test_name_two', False)
     list_result2 = channels.channels_list(user2['token'])
     assert list_result2[0]['channel_id'] == 2
     assert len(list_result2[0]['name']) < 20
 
-    data.clear_data()
+    other.clear()
 
 # Unsuccessful
+def test_channels_create_invalid_token():
+    other.clear()
+
+    user = auth.auth_register('testmail12321@gmail.com', 'passwordpass1', 'first_name', 'last_name')
+    with pytest.raises(AccessError) as e:
+        assert channels.channels_create('invalid_token', 'test_name1', True)
+
+    user2 = auth.auth_register('mailtest12321@gmail.com', 'passwpassword1', 'first_name', 'last_name')
+    with pytest.raises(AccessError) as e:
+        assert channels.channels_create('another_invalid_token', 'test_name1', False)
+
+    other.clear()
+
 def test_channels_create_name_error():
-    data.clear_data()
-    
+    other.clear()
+
     user = auth.auth_register('testmail123@gmail.com', 'passwordpass', 'first_name', 'last_name')
-    user = auth.auth_login('testmail123@gmail.com', 'passwordpass')
     with pytest.raises(InputError) as e:
         assert channels.channels_create(user['token'], 'test_name_12345678910x', True)
 
     user2 = auth.auth_register('mailtest123@gmail.com', 'passwpassword', 'first_name', 'last_name')
-    user2 = auth.auth_login('mailtest123@gmail.com', 'passwpassword')
     with pytest.raises(InputError) as e:
         assert channels.channels_create(user2['token'], 'test_name_0987654321x', False)
 
-    data.clear_data()
+    other.clear()
+
