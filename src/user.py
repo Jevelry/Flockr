a83a0ab410105f30bd51c7135f0450d43523a3f2
Kevin <z5311917@ad.unsw.edu.au>
2020-10-14 15:18:28 +1100

    Finished writing tests and implementatio for user_profile and user_profile_setname

--- src/user.py
+++ src/user.py
@@ -1,11 +1,78 @@
 """
-Replace this with your own docstring.
-I just want to pass pylint
+data(data.py): Gives access to global data variable
+error(error.py): Gives access to error classes
+auth(auth.py): Gives access to check valid names and emails
+channel(channel.py): Gives access to valid_u_id function
+re(regex): Gives access to regex for valid_email
 """
+from auth import existing_email, valid_name
+import data
+from error import InputError, AccessError
+from channel import valid_u_id
+import re
+
+def valid_token(token):
+    """
+    Determine whether supplied token is valid
+
+    Parameters:
+        token(string): An authorisation hash
+
+    Returns:
+        Boolean based on whether token is valid
+    """
+    for user in data.data["logged_in"]:
+        if user["token"] == token:
+            return True  
+    return False  
+
+
+def get_uid(token):
+    """
+    Get users u_id from token
+    
+    Parameters:
+        token(string): An authorisation hash
+
+    Returns:
+        u_id(int) 
+    """
+    for user in data.data["logged_in"]:
+        if user["token"] == token:
+            return user["u_id"]
+
+
+
+
 def user_profile(token, u_id):
     """
-    Replace this with your own docstring.
-    I just want to pass pylint
+    For a valid user, returns information about their user_id, 
+    email, first name, last name, and handle
+    
+    Parameters:
+        token(string): An authorisation hash
+        u_id(int): Identifier for user
+        
+    Returns:
+        Dictionary with information about user
+    """
+    #Check for valid token
+    if valid_token(token) == False:
+        raise AccessError
+    #Check for valid u_id
+    if valid_u_id(u_id) == False:
+        raise InputError
+    #Everything valid, proceed with getting profile details
+    user = {}
+    for users in data.data["users"]:
+        if users["u_id"] == u_id:
+            user["u_id"] = users["u_id"]
+            user["email"] = users["email"]
+            user["name_first"] = users["first"]
+            user["name_last"] = users["last"]
+            user["handle_str"] = users["handle"]
+        return user      
+
     """
     return {
         'user': {
@@ -18,30 +85,36 @@ def user_profile(token, u_id):
             'id' : u_id + 1 # Delete this line
         },
     }
-
+    """
+    
+    
+    
+    
+    
 def user_profile_setname(token, name_first, name_last):
     """
-    Replace this with your own docstring.
-    I just want to pass pylint
+    For a valid user, update their name
+    
+    Parameters:
+        token(string): An authorisation hash
+        name_first(string): new first name
+        name_last(stirng): new last name
+        
+    Returns:
+        Nothing
     """
-    return (token, name_first, name_last) # Delete this line
-    # return {
-    # }
-
-def user_profile_setemail(token, email):
-    """
-    Replace this with your own docstring.
-    I just want to pass pylint
-    """
-    return (token, email) # Delete this line
-    # return {
-    # }
-
-def user_profile_sethandle(token, handle_str):
-    """
-    Replace this with your own docstring.
-    I just want to pass pylint
-    """
-    return (token, handle_str) # Delete this line
-    # return {
-    # }
+    #Check for valid token
+    if not valid_token(token):
+        raise AccessError
+    #Check for valid name
+    if not valid_name(name_first, name_last):
+        raise InputError    
+    #Everything valid, proceed with changing name
+    u_id = get_uid(token)
+    for users in data.data["users"]:
+        if users["u_id"] == u_id: 
+            users["first"] = name_first
+            users["last"] = name_last  
+        
+    
+    
--- src/user_test.py
+++ src/user_test.py
@@ -6,6 +6,7 @@
     other(other.py): Gives access to other.clear command
     error(error.py): Gives access to error classes
     user(user.py): Gives access to user functions
+    data(data.py): Gives access to global data variable
 """
 import pytest
 import auth
@@ -14,24 +15,49 @@ import channels
 import user
 import other
 from error import InputError, AccessError
+import data
 
+def check_name_change(u_id, first, last):
+    """
+    Checks if set_name has successfully changed name
+
+    Parameters:
+        user(int): u_id (Identifier for user)
+        first: new first name
+        last: new last name
+
+    Returns:
+        Nothing
+    """
+    for user in data.data['users']:
+        if user['u_id'] == u_id:
+            assert user["first"] == first
+            assert user["last"] == last
+            break
+            
+            
 #USER_PROFILE TESTS
 #SUCCESSFUL
-
 def test_user_profile_request_self():
+    """
+    Testing successful uses of user_profile
+    focusing on request oneselves profile
+    """
     user1 = auth.auth_register("kevin.huang@gmail.com", "nice123", "Kevin", "Huang")
     user1_profile = {
             'u_id': 1,
             'email': 'kevin.huang@gmail.com',
             'name_first': 'Kevin',
             'name_last': 'Huang',
-            'handle_str': 'kevinhuang',  
+            'handle_str': 'KevinHuang',  
         }
     assert user.user_profile(user1["token"], user1["u_id"]) == user1_profile
-
-
-
+    other.clear()
 def test_user_profile_request_others():
+    """
+    Testing Successful uses of user_profile 
+    focusing on requesting other users profiles
+    """
     user1 = auth.auth_register("kevin.huang@gmail.com", "nice123", "Kevin", "Huang")
     user2 = auth.auth_register("lucyjang@gmail.com", "lj1234", "Lucy", "Jang")
     user1_profile = {
@@ -39,18 +65,26 @@ def test_user_profile_request_others():
             'email': 'kevin.huang@gmail.com',
             'name_first': 'Kevin',
             'name_last': 'Huang',
-            'handle_str': 'kevinhuang',  
+            'handle_str': 'KevinHuang',  
         }
     assert user.user_profile(user2["token"], user1["u_id"]) == user1_profile
-
+    other.clear()
 #Unsuccessful
 def test_user_profile_invalid_token():
+    """
+    Testing unsuccessful uses of user_profile 
+    focusing on invalid tokens
+    """
     user1 = auth.auth_register("kevin.huang@gmail.com", "nice123", "Kevin", "Huang")
     with pytest.raises(AccessError):
         assert user.user_profile("invalid_token", user1["u_id"])
     other.clear()
 
 def test_user_profile_invalid_uid():
+    """
+    Testing unsuccessful uses of user_profile     
+    focusing on invalid u_id
+    """
     user1 = auth.auth_register("kevin.huang@gmail.com", "nice123", "Kevin", "Huang")
     invalid_uid = 9
     with pytest.raises(InputError):
@@ -61,70 +95,78 @@ def test_user_profile_invalid_uid():
 #SUCCESSFUL
 
 def test_user_setname_valid_name():
+    """
+    Testing successful uses of user_profile_setname
+    focusing on valid names
+    """
+    user1 = auth.auth_register("kevin.huang@gmail.com", "nice123", "Kevin", "Huang")
+    user.user_profile_setname(user1["token"], "Awesome", "Joey")
+    check_name_change(user1["u_id"], "Awesome", "Joey")
+    other.clear()
+    
+def test_user_setname_lastname_only():
+    """
+    Testing successful uses of user_profile_setname
+    focusing on changing lastname only
+    """
+    user1 = auth.auth_register("kevin.huang@gmail.com", "nice123", "Kevin", "Huang")
+    user.user_profile_setname(user1["token"], "Kevin", "Awesome")
+    check_name_change(user1["u_id"], "Kevin", "Awesome")
+    other.clear()
 
+def test_user_setname_firstname_only():
+    """
+    Testing successful uses of user_profile_setname
+    focusing on changing firstname only
+    """
+    user1 = auth.auth_register("kevin.huang@gmail.com", "nice123", "Kevin", "Huang")
+    user.user_profile_setname(user1["token"], "Awesome", "Huang")
+    check_name_change(user1["u_id"], "Awesome", "Huang")
+    other.clear()
 
-
-def test_user_setname_twice():
-
-
-
+def test_user_setname_samename():
+    """
+    Testing successful uses of user_profile_setname
+    focusing on changing to same existing name
+    """
+    user1 = auth.auth_register("kevin.huang@gmail.com", "nice123", "Kevin", "Huang")
+    user.user_profile_setname(user1["token"], "Kevin", "Huang")
+    check_name_change(user1["u_id"], "Kevin", "Huang")
+    other.clear()
+    
 #Unsuccessful
 def test_user_setname_invalid_token():
-
-
-
+    """
+    Testing unsuccessful uses of user_profile_setname
+    focusing on invalid tokens
+    """
+    user1 = auth.auth_register("kevin.huang@gmail.com", "nice123", "Kevin", "Huang")
+    with pytest.raises(AccessError):
+        assert user.user_profile_setname("invalid_token", "new", "name")
+    other.clear()
 
 def test_user_setname_invalid_firstname():
-
-"""
-do over 50 characters and 0 characters
-"""
-
-
+    """
+    Testing unsuccessful uses of user_profile_setname
+    focusing on invalid firstnames
+    """
+    user1 = auth.auth_register("kevin.huang@gmail.com", "nice123", "Kevin", "Huang")
+    with pytest.raises(InputError):
+        assert user.user_profile_setname(user1["token"], "iwbliueblaiublvuaeblriualerugbeiurbgliuebrgiubguiea", "name")
+        
+        assert user.user_profile_setname(user1["token"], "", "name")
+    other.clear()
+    
 def test_user_setname_invalid_lastname():
+    """
+    Testing unsuccessful uses of user_profile_setname
+    focusing on invalid lastnames
+    """
+    user1 = auth.auth_register("kevin.huang@gmail.com", "nice123", "Kevin", "Huang")
+    with pytest.raises(InputError):
+        assert user.user_profile_setname(user1["token"], "new", "niwbliueblaiublvuaeblriualerugbeiurbgliuebrgiubguiea")
 
+        assert user.user_profile_setname(user1["token"], "new", "")
+    other.clear()
 
-
-#USER_PROFILE_SETEMAIL TESTS
-#SUCCESSFUL
-def test_user_setemail_valid_email():
-
-
-
-#Unsuccessful
-def test_user_setemail_invalid_token():
-
-
-
-
-
-def test_user_setemail_invalid_email():
-
-
-
-def test_user_setemail_email_taken():
-
-
-
-
-#USER_PROFILE_SETHANDLE TESTS
-#SUCCESSFUL
-def test_user_sethandle_valid_handle():
-
-
-
-#Unsuccessful
-def test_user_sethandle_invalid_token():
-
-
-
-
-def test_user_sethandle_invalid_handle():
-
-
-
-def test_user_sethandle_handle_taken(): 
-
-
-
-
+    
