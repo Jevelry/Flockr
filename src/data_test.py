7c19006e80441a306d9e671f0052f37bd3cec92d
Kevin <z5311917@ad.unsw.edu.au>
2020-10-22 12:53:25 +1100

    added data_test file

---
+++ src/data_test.py
@@ -0,0 +1,94 @@
+"""
+Tests for data.py functions
+"""
+from error import AccessError, InputError
+import data
+import auth
+import channels
+import other
+
+def test_get_user_with():
+
+
+def test_get_user_info():
+
+
+def test_update_user():
+
+
+def test_update_user_channel_list():
+
+
+def test_register_user():
+
+
+def test_login_user():
+
+
+def test_check_logged_in():
+
+
+def test_logout_user():
+
+
+def test_get_channel_info():
+
+
+def test_channel_add_member():
+
+
+def test_check_user_in_channel():
+
+
+def test_check_channel_owner():
+
+
+def test_channel_add_owner():
+
+
+def test_channel_remove_member():
+
+
+def test_channel_remove_owner():
+
+
+def test_get_message_num():
+
+
+def test_get_num_users():
+
+
+def test_make_message_id():
+
+
+def test_channels_list_user():
+
+
+def test_channels_list_all():
+
+
+def test_get_num_channels():  
+
+
+def test_channel_create():
+
+
+def test_find_channel():
+
+
+def test_get_message():
+
+
+def test_add_message():
+
+
+def test_remove_message():
+
+
+def test_edit_message():
+
+
+def test_user_list():
+
+
+def test_change_permission():
