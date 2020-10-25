16a9e02f766e94522c75da0b1e8f75d13377f05c
Elliot Rotenstein <elliot@balgara.com>
2020-10-26 09:53:51 +1100

    Fixed pylint. Changed the user fixture in other_test.py to user1 to avoid clashes with importing user module

--- src/other_test.py
+++ src/other_test.py
@@ -18,7 +18,7 @@ import user
 from error import InputError, AccessError
 
 @pytest.fixture
-def user():
+def user1():
     """
     Pytest fixture that automatically registers a user and returns their info
     """
@@ -28,12 +28,12 @@ def user():
 
 # Tests for users_all
 # Sucessful
-def test_users_all_sucess(user):
+def test_users_all_sucess(user1):
     """
     Tests successful uses of users_all
     """
 
-    user_result = other.users_all(user['token'])
+    user_result = other.users_all(user1['token'])
     assert user_result['users'][0]['u_id'] == 1
     assert user_result['users'][0]['email'] == 'testmail@gmail.com'
     assert user_result['users'][0]['name_first'] == 'first_name'
@@ -47,7 +47,7 @@ def test_users_all_sucess(user):
     other.clear()
 
 # Unsucessful
-def test_users_all_invalid_token(user):
+def test_users_all_invalid_token(user1):
     """
     Tests unsuccessful uses of users_all,
     focusing on invalid tokens
@@ -60,60 +60,60 @@ def test_users_all_invalid_token(user):
 
 # Tests for admin_userpermission_change
 # Sucessful
-def test_admin_userpermission_change_success(user):
+def test_admin_userpermission_change_success(user1):
     """
     Tests successful uses of admin_userpermission_change
     """
     user2 = auth.auth_register('closet@yahoo.com.au', 'closetset12', 'first_name', 'last_name')
-    assert other.admin_userpermission_change(user['token'], user2['u_id'], 1) == {}
+    assert other.admin_userpermission_change(user1['token'], user2['u_id'], 1) == {}
 
     other.clear()
 
-def test_admin_userpermission_change_revert_success(user):
+def test_admin_userpermission_change_revert_success(user1):
     """
     Tests successful uses of admin_userpermission_change,
     focusing on reverting changes
     """
     user2 = auth.auth_register('pancake@yahoo.com.au', 'honeysyrup2', 'first_name', 'last_name')
-    assert other.admin_userpermission_change(user['token'], user2['u_id'], 1) == {}
-    assert other.admin_userpermission_change(user['token'], user2['u_id'], 2) == {}
+    assert other.admin_userpermission_change(user1['token'], user2['u_id'], 1) == {}
+    assert other.admin_userpermission_change(user1['token'], user2['u_id'], 2) == {}
 
     other.clear()
 
 # Unsucessful
-def test_admin_userpermission_change_invalid_token(user):
+def test_admin_userpermission_change_invalid_token(user1):
     """
     Tests unsuccessful uses of admin_userpermission_change,
     focusing on invalid tokens
     """
 
     with pytest.raises(AccessError):
-        assert other.admin_userpermission_change('invalid_token', user['u_id'], 1)
+        assert other.admin_userpermission_change('invalid_token', user1['u_id'], 1)
 
     other.clear()
 
-def test_admin_userpermission_change_not_owner(user):
+def test_admin_userpermission_change_not_owner(user1):
     """
     Tests unsuccessful uses of admin_userpermission_change,
     focusing on the 'admin' not having owner privileges
     """
     user2 = auth.auth_register('chair@gmail.com', 'chair202', 'first_name', 'last_name')
     with pytest.raises(AccessError):
-        assert other.admin_userpermission_change(user2['token'], user['u_id'], 2)
+        assert other.admin_userpermission_change(user2['token'], user1['u_id'], 2)
 
     other.clear()
 
-def test_admin_userpermission_change_invalid_uid(user):
+def test_admin_userpermission_change_invalid_uid(user1):
     """
     Testing unsuccessful uses of admin_userpermission_change,
     focusing on invalid user ids
     """
     with pytest.raises(InputError):
-        assert other.admin_userpermission_change(user['token'], 'invalid_uid', 1)
+        assert other.admin_userpermission_change(user1['token'], 'invalid_uid', 1)
 
     other.clear()
 
-def test_admin_userpermission_change_invalid_permission(user):
+def test_admin_userpermission_change_invalid_permission(user1):
     """
     Testing unsuccessful uses of admin_userpermission_change,
     focusing on invalid permission id
@@ -122,80 +122,80 @@ def test_admin_userpermission_change_invalid_permission(user):
     user2 = auth.auth_register('chair@gmail.com', 'chair202', 'first_name', 'last_name')
 
     with pytest.raises(InputError):
-        assert other.admin_userpermission_change(user['token'], user2['u_id'], 3)
+        assert other.admin_userpermission_change(user1['token'], user2['u_id'], 3)
 
     other.clear()
 
 
 # Tests for search
 # Sucessful
-def test_search_results_single(user):
+def test_search_results_single(user1):
     """
     Tests successful uses of search,
     focusing on single message results
     """
 
-    test_channel = channels.channels_create(user['token'], 'test_name', True)
+    test_channel = channels.channels_create(user1['token'], 'test_name', True)
     test_message = 'This is a test message.'
-    message.message_send(user['token'], test_channel['channel_id'], test_message)
-    find_message = other.search(user['token'], 'is a test')
+    message.message_send(user1['token'], test_channel['channel_id'], test_message)
+    find_message = other.search(user1['token'], 'is a test')
 
     assert find_message['messages'][0]['message_id'] == 1
-    assert find_message['messages'][0]['u_id'] == 1
+    assert find_message['messages'][0]['u_id'] == user1['u_id']
     assert find_message['messages'][0]['message'] == 'This is a test message.'
 
     other.clear()
 
-def test_search_results_multiple(user):
+def test_search_results_multiple(user1):
     """
     Tests successful uses of search,
     focusing on queries that return multiple messages
     """
 
     user2 = auth.auth_register('rockyroad@hotmail.com', 'chocfudge222', 'first_name', 'last_name')
-    test_channel = channels.channels_create(user['token'], 'test_name', True)
+    test_channel = channels.channels_create(user1['token'], 'test_name', True)
     channel.channel_join(user2['token'], test_channel['channel_id'])
 
     test_message = 'This is a test message.'
     test_message2 = 'This message is a test right?'
     test_message3 = 'Yes it is a test.'
-    message.message_send(user['token'], test_channel['channel_id'], test_message)
+    message.message_send(user1['token'], test_channel['channel_id'], test_message)
     message.message_send(user2['token'], test_channel['channel_id'], test_message2)
-    message.message_send(user['token'], test_channel['channel_id'], test_message3)
-    find_message = other.search(user['token'], 'is a test')
+    message.message_send(user1['token'], test_channel['channel_id'], test_message3)
+    find_message = other.search(user1['token'], 'is a test')
 
     assert find_message['messages'][0]['message_id'] == 1
-    assert find_message['messages'][0]['u_id'] == 1
+    assert find_message['messages'][0]['u_id'] == user1['u_id']
     assert find_message['messages'][0]['message'] == 'This is a test message.'
 
     assert find_message['messages'][1]['message_id'] == 3
-    assert find_message['messages'][1]['u_id'] == 1
+    assert find_message['messages'][1]['u_id'] == user1['u_id']
     assert find_message['messages'][1]['message'] == 'Yes it is a test.'
 
     other.clear()
 
-def test_search_no_results(user):
+def test_search_no_results(user1):
     """
     Tests on successful uses of search,
     focusing on querise that don't return any strings
     """
-    channels.channels_create(user['token'], 'test_name', True)
+    channels.channels_create(user1['token'], 'test_name', True)
 
-    find_message = other.search(user['token'], 'is a test')
+    find_message = other.search(user1['token'], 'is a test')
     assert find_message['messages'] == []
 
     other.clear()
 
 # Unsucessful
-def test_search_invalid_token(user):
+def test_search_invalid_token(user1):
     """
     Tests unsuccessful uses of search,
     focusing on invalid tokens
     """
 
-    test_channel = channels.channels_create(user['token'], 'test_name', True)
+    test_channel = channels.channels_create(user1['token'], 'test_name', True)
     test_message = 'We love cats and dogs'
-    message.message_send(user['token'], test_channel['channel_id'], test_message)
+    message.message_send(user1['token'], test_channel['channel_id'], test_message)
     
     with pytest.raises(AccessError):
         assert other.search('invalid_token', 'is a test')
